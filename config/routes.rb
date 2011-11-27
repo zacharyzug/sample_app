f5ab42ca537b536c93e312cefc52af284bb3195b
Zachary Katz <zachary@post.harvard.edu>
2011-11-27 11:56:01 -0500

    Removed Rails Page and a bunch of stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 SampleApp::Application.routes.draw do
-  get "pages/home"
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+  root :to => "pages#home"
+end  
 
-  get "pages/contact"
-
-  get "pages/about" 
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -60,4 +61,4 @@ SampleApp::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
-end
+
